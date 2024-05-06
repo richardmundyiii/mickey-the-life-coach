a8a17bf3240e0b15800c68b23338a34e9c5d1190
Madison Taylor <mundy.richard@icloud.com>
2024-05-06 06:51:30 -0700

    remove signup component

--- src/pages/AdminPage/AdminPage.jsx
+++ src/pages/AdminPage/AdminPage.jsx
@@ -24,7 +24,7 @@ export default function AdminPage() {
     }
   };
   return (
-    <Authenticator hideSignUp={false}>
+    <Authenticator hideSignUp={true}>
       {({ signOut }) => (
         <Container fluid>
           <Row className="d-none">
